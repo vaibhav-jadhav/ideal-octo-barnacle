1d1d186ab45fb9cb2fe9cd00353d0e32586f843b
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2020-11-22 23:07:33 +0530

    added all

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="aligncent">
+        <h1>Site Under Developement</h1>
+    </div>
+  );
+}
+
+export default App;
