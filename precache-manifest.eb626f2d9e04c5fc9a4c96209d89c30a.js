fd9786402cee06e513fab68a99392ba1f59e8353
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2020-11-22 23:39:43 +0530

    Updates

---
+++ precache-manifest.eb626f2d9e04c5fc9a4c96209d89c30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11838007348c29ad39f3eb82114e365",
+    "url": "/ideal-octo-barnacle/index.html"
+  },
+  {
+    "revision": "fe8759c261e378f7f9a5",
+    "url": "/ideal-octo-barnacle/static/css/main.d5ccc1fe.chunk.css"
+  },
+  {
+    "revision": "24ef72953096de2a33b2",
+    "url": "/ideal-octo-barnacle/static/js/2.8d1a3f2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ideal-octo-barnacle/static/js/2.8d1a3f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8759c261e378f7f9a5",
+    "url": "/ideal-octo-barnacle/static/js/main.52d90c8c.chunk.js"
+  },
+  {
+    "revision": "6cf0a502ef6cb4c5182f",
+    "url": "/ideal-octo-barnacle/static/js/runtime-main.29c15da8.js"
+  }
+]);
